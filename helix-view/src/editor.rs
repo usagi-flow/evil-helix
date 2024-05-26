254582582f26fdce6a04ab7adf0b5f65a76443e5
兎の流れ <2804556+usagi-flow@users.noreply.github.com>
2024-05-26 12:52:24 +0200

    feat(evil): make find_char commands evil (#14)

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -744,6 +744,8 @@ impl StatusLineConfig {
             ],
             separator: String::from("│"),
             mode: ModeConfig::default_evil(),
+            diagnostics: vec![Severity::Warning, Severity::Error],
+            workspace_diagnostics: vec![Severity::Warning, Severity::Error],
         }
     }
 }
