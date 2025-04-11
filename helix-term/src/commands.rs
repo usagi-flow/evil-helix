589814f8114d806a9e4da61d319af96b0d33cc8f
Thomas Miedema <thomasmiedema@gmail.com>
2025-04-11 17:39:26 +0200

    fix(evil): make `w`, `W`, `e`, `E`, `b` and `B` in normal and select mode behave exactly like Vim (#69)
    * Simplify evil_move_word_impl, fixing bugs
    
    The function `evil_move_word_impl` can be simplified, thereby fixing the
    following two bugs in the process:
    
    1. In select mode, the key sequence `wb` would result in a selection,
       instead of being a no-op as in Vim.
    
    2. The functions `evil_next_word_end` and `evil_next_long_word_end`
       (mapped to 'e' and 'E') in normal mode would make the cursor land on
       the space after the word, instead of the last character of the word.
    
    The only problem with this commit is that now the functions
    `evil_next_word_start` and `evil_next_long_word_start` (mapped to `w`
    and `W`) don't behave correctly anymore, making the cursor land
    on the space before the next word. We'll fix it in the next commit.
    
    * Fix `w` and `W`
    
    1. Space before word: when the cursor is on the space before a word, and
       you hit `w`, Vim will move the cursor to this word, but evil-helix
       would skip the word and move to the _next_ word.
    
    2. Punctuation: when the cursor is on the letter 'f' in "foo.bar qux",
       and you hit `w` twice, Vim will move the cursor to the letter 'b',
       but evil-helix would skip to the letter 'q'.
    
       This bug has the same underlying cause as the previous one, and the
       fix for both is to move the cursor one character to the left before
       doing the word move.
    
    3. Regression: the previous commit, simplifying `evil_move_word_impl`,
       introduced a bug where `w` would make the cursor land on the space
       before the next word, instead of _on_ the next word. Fix it by moving
       the cursor one character to the right again after the word move.
    
    4. Newlines: when the cursor is on the 'f' in "foo\nbar", and you hit
       `w`, Vim will move the cursor to the letter 'b' (so to the next
       line), but evil-helix would move the cursor onto the newline
       character. See the changes in `reached_target` for the fix.
    
    Fixes #44

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -7263,29 +7263,14 @@ fn evil_move_word_impl<F>(cx: &mut Context, move_fn: F)
 where
     F: Fn(RopeSlice, Range, usize) -> Range,
 {
-    let count = cx.count();
-    let (view, doc) = current!(cx.editor);
-    let text = doc.text().slice(..);
-
-    let selection = doc.selection(view.id).clone().transform(|range| {
-        let old_head = range.head;
-        let old_anchor = range.anchor;
-        let mut new_range = move_fn(text, range, count);
-
-        new_range.anchor = match cx.editor.mode {
-            // In select mode, use a sticky anchor and move the head only;
-            // keeping in mind that in select mode, with a single char selected,
-            // the anchor typically points *before* the character.
-            Mode::Select if new_range.head < old_head => old_head.max(old_anchor),
-            Mode::Select => old_head.min(old_anchor),
+    match cx.editor.mode {
+        Mode::Select => extend_word_impl(cx, move_fn),
+        _ => {
             // When not in select mode, just move the cursor and do not select
-            _ => new_range.head,
-        };
-
-        return new_range;
-    });
-
-    doc.set_selection(view.id, selection);
+            move_word_impl(cx, move_fn);
+            collapse_selection(cx)
+        }
+    }
 }
 
 fn evil_prev_word_start(cx: &mut Context) {
@@ -7295,9 +7280,7 @@ fn evil_prev_word_start(cx: &mut Context) {
 }
 
 fn evil_next_word_start(cx: &mut Context) {
-    // TODO: evil-specific implementation in evil.rs
-    evil_move_word_impl(cx, movement::move_next_word_start);
-    //EvilCommands::next_word_start(cx);
+    evil_move_word_impl(cx, EvilCommands::move_next_word_start);
 }
 
 fn evil_next_word_end(cx: &mut Context) {
@@ -7311,8 +7294,7 @@ fn evil_prev_long_word_start(cx: &mut Context) {
 }
 
 fn evil_next_long_word_start(cx: &mut Context) {
-    // TODO: evil-specific implementation in evil.rs
-    evil_move_word_impl(cx, movement::move_next_long_word_start);
+    evil_move_word_impl(cx, EvilCommands::move_next_long_word_start);
 }
 
 fn evil_next_long_word_end(cx: &mut Context) {
